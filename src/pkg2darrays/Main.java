1cd36a41713c9e7898f19da04f8345c13de9a42b
Sam Weiss <114678287+sweisscct@users.noreply.github.com>
2024-01-17 20:22:06 +0000

    Array type and default value examples

--- src/pkg2darrays/Main.java
+++ src/pkg2darrays/Main.java
@@ -20,6 +20,26 @@ public class Main {
         int[][] AnotherArray = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12} }; // 4x3
         
         int[][] JaggedArray = { {1, 2, 3}, {4, 5}, {6, 7, 8, 9} };
+        
+        int[][] NotSpecifyingSizes = new int[5][];
+        
+        System.out.println(AnotherArray[1][0]); // 4
+        System.out.println(TwoDArray[1][0]); // 0 (int default value)
+//        System.out.println(NotSpecifyingSizes[1][0]); // Second (inner) array is unallocated, so there is nothing to read.
+        
+        double[] doubleArray = new double[10];
+        System.out.println(doubleArray[4]);
+        
+        boolean[] booleanArray = new boolean[10];
+        System.out.println(booleanArray[4]);
+        
+        char[] charArray = new char[6];
+        System.out.println(charArray[3]);
+        
+        String[] stringArray = new String[10];
+        System.out.println(stringArray[4]);
+        
+        
     }
     
 }
